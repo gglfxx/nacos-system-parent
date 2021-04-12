e5e3c535da39bc07426117869c299777523ac00a
guiguoliang <584961991@qq.com>
2021-04-12 17:33:25 +0800

    1、删除资源服务 2、修改依赖pom

--- auth-system-web/src/main/java/com/auth/web/config/AuthorizationServerConfig.java
+++ auth-system-web/src/main/java/com/auth/web/config/AuthorizationServerConfig.java
@@ -8,37 +8,53 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import javax.sql.DataSource;
 
 /**
- * oauth2 授权服务配置
+ * OAuth2.0配置
  */
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
     private DataSource dataSource;
 
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    /**
+     * AuthorizationServerSecurityConfigurer继承SecurityConfigurerAdapter.
+     * 也就是一个 Spring Security安全配置提供给AuthorizationServer去配置AuthorizationServer的端点（/oauth/****）的安全访问规则、过滤器Filter
+     * @throws Exception
+     */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        super.configure(security);
+        security.allowFormAuthenticationForClients();
+        security.tokenKeyAccess("isAuthenticated()");
     }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        //配置客户端存储到db 代替原来得内存模式
+        //通过数据库去读取客户端配置 默认读取表oauth_client_details
+        //若涉及到业务情形 则手动定制化JdbcClientDetailsService
         clients.jdbc(dataSource);
     }
 
+    /**
+     * AuthorizationServerEndpointsConfigurer其实是一个装载类，
+     * 装载Endpoints所有相关的类配置（AuthorizationServer、TokenServices、TokenStore、ClientDetailsService、UserDetailsService）
+     * @param endpoints
+     * @throws Exception
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.accessTokenConverter(jwtAccessTokenConverter());
-        endpoints.tokenStore(jwtTokenStore());
+        endpoints.accessTokenConverter(jwtAccessTokenConverter())
+                .tokenStore(jwtTokenStore()).reuseRefreshTokens(true);
     }
 
     @Bean
@@ -49,7 +65,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("cjs");   //  Sets the JWT signing key
+        jwtAccessTokenConverter.setSigningKey("token");   //  Sets the JWT signing key
         return jwtAccessTokenConverter;
     }
+
 }
